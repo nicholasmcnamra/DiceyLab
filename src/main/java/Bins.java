a98db42d8cd2f1c1472935abb7b23315ea8bfcb4
nicholasmcnamra <nikomcnamara@gmail.com>
2024-03-12 11:37:44 -0400

    simulation tests and starting on bins creation

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,23 @@
+import java.util.HashMap;
 
 public class Bins {
+   private HashMap<Integer, Integer> bin = new HashMap<>();
 
+   Simulation sim = new Simulation(2, 100);
+   public Bins(HashMap<Integer, Integer> bin) {
+       this.bin = bin;
+   }
+
+   public Bins() {
+
+   }
+
+//    public int trackResults(int numberOfTosses) {
+//       int timesRolled = 0;
+//       Integer newBin = null;
+//       for (int i = 0; i < numberOfTosses; i++) {
+//           newBin = bin.put(sim.runSimulation(), timesRolled++);
+//       }
+//       return newBin;
+//    }
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,18 +1,30 @@
+import java.util.HashMap;
+
 public class Simulation {
     private int numberOfDice;
     private int numberOfTosses;
     Dice dice = new Dice();
+    Bins bin = new Bins();
+
     public Simulation(int numberOfDice, int numberOfTosses) {
         this.numberOfDice = numberOfDice;
         this.numberOfTosses = numberOfTosses;
     }
 
+
+
     public int runSimulation() {
         int total = 0;
         for (int i = 0; i < numberOfTosses; i++) {
-            total += dice.rollDice(numberOfDice);
-            System.out.println("Dice total:  " + total);
-            total = 0;
+            if (numberOfTosses <= 1) {
+                total += dice.rollDice(numberOfDice);
+//            System.out.println("Dice total:  " + total);}
+                }
+            else  {
+                total += dice.rollDice(numberOfDice);
+//            System.out.println("Dice total:  " + total);
+                total = 0;
+            }
         }
         return total;
     }
